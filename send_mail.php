37ba78a2ad59e965b7644a089b023540ea8d6779
Polina Makarenko <polinamakarenko.com@gmail.com>
2023-06-23 23:17:25 +0200

    feat: add php file

---
+++ send_mail.php
@@ -0,0 +1,40 @@
+<?php 
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$name = $_POST['name'];
+$message = $_POST['message'];
+$email = $_POST['email'];
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'ultra_locator@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = '$dfhifdf2023'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+
+$mail->setFrom('ultra_locator@mail.ru'); // от кого будет уходить письмо?
+$mail->addAddress('polinamakarenko.com@gmail.com');     // Кому будет уходить письмо 
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'New application (Ultra Locator)';
+$mail->Body    = '' .$name . ' sent a message: ' .$message. '<br>E-mail: ' .$email;
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    echo 'Error';
+} else {
+    echo ('The application has been sent!');
+}
+?>
